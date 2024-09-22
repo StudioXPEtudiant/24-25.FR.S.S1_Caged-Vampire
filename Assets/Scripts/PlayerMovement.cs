49e1035a18523cdde4a3a9a5df8d27930f77d61e
Théo FONTANY <theofontany.pro@gmail.com>
2024-09-22 22:35:12 +0200

    Add Input System | Fix tilemap collision bug

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,43 +1,49 @@
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour {
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float runSpeed = 7f;
     
     [Header("Jump")]
     [SerializeField] private float jumpForce = 1f;
+    [SerializeField] private float groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask groundLayer;
     
     private Transform _groundCheck;
     private Rigidbody2D _rigidbody2D;
     private SpriteRenderer _spriteRenderer;
+
+    private InputAction _move;
+    private InputAction _jump;
+    private InputAction _run;
     
     private void Start() {
         _groundCheck = transform.Find("GroundCheck");
+        
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        _move = InputSystem.actions.FindAction("Move");
+        _jump = InputSystem.actions.FindAction("Jump");
+        _jump.started += Jump;
     }
 
-    private void Update() {
-        var h = Input.GetAxis("Horizontal");
-        if(h != 0) _spriteRenderer.flipX = h < 0;
-        
-        if(Input.GetKeyDown(KeyCode.W)) Jump();
-        
-        // Movement
-        if(Input.GetKey(KeyCode.LeftShift)) Move(h, runSpeed);
-        else Move(h, moveSpeed);
-    }
-    
-    private void Move(float value, float speed) {
-        var movement = new float3(value, 0f, 0f);
-        transform.Translate(movement * speed * Time.deltaTime);
+    private void FixedUpdate() {
+        var direction = _move.ReadValue<Vector2>().x;
+        if(direction != 0) Move(direction);
     }
 
-    private void Jump() {
-        if(!Physics2D.OverlapCircle(_groundCheck.position, 0.2f, groundLayer)) return;
+    private void Move(float direction) {
+        _rigidbody2D.velocity = new float2(direction * moveSpeed, _rigidbody2D.velocity.y);
+        _spriteRenderer.flipX = direction < 0;
+    }
+
+    private void Jump(InputAction.CallbackContext context) {
+        if(!IsGround()) return;
         _rigidbody2D.AddForce(new float2(0f, jumpForce), ForceMode2D.Impulse);
     }
+
+    private bool IsGround() => Physics2D.OverlapCircle(_groundCheck.position, groundCheckRadius, groundLayer);
 }
